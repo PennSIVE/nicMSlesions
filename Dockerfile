92ff1a498587e844d335bbd56620e8f6161796f5
Tim <tim.terf@gmail.com>
2020-06-22 09:46:11 -0400

    added minimal functionality

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
-FROM bids/base_fsl
-
-# Install python, nibabel and numpy (nibabel>=2.1 requires python>=3.5, ubuntu trusty has only python 3.4)
+FROM neurodebian:buster
 RUN apt-get update && \
-    apt-get install -y python3 python3-pip python3-numpy && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm git python3 python3-pip python3-numpy python python-pip python-tk && \
+    git clone https://github.com/sergivalverde/nicMSlesions.git && \
+    cd nicMSlesions && \
     pip3 install nibabel==2.0 && \
-    apt-get remove -y python3-pip && \
+    pip install tensorflow==1.6.0 && \
+    pip install -r requirements.txt && \
+    npm install -g bids-validator@0.19.2 && \
+    apt-get remove -y python-pip python3-pip git npm && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
+# RUN ls /nicMSlesions | grep batch.py$ | xargs -I {} sed -i "s#CURRENT_PATH, 'config'#'/', 'config'#g" /nicMSlesions/{}
 ENV PYTHONPATH=""
-
+COPY config.py /nicMSlesions/utils/load_options.py
 COPY run.py /run.py
-
 COPY version /version
-
 ENTRYPOINT ["/run.py"]
--- run.py
+++ run.py
@@ -2,12 +2,13 @@
 import argparse
 import os
 import subprocess
-import nibabel
-import numpy
+import tempfile
+import shutil
 from glob import glob
 
 __version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'version')).read()
+
 
 def run(command, env={}):
     merged_env = os.environ
@@ -22,9 +23,11 @@ def run(command, env={}):
         if line == '' and process.poll() != None:
             break
     if process.returncode != 0:
-        raise Exception("Non zero return code: %d"%process.returncode)
+        raise Exception("Non zero return code: %d" % process.returncode)
 
-parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
+
+parser = argparse.ArgumentParser(
+    description='nicMSlesions entrypoint script.')
 parser.add_argument('bids_dir', help='The directory with the input dataset '
                     'formatted according to the BIDS standard.')
 parser.add_argument('output_dir', help='The directory where the output files '
@@ -35,22 +38,24 @@ parser.add_argument('analysis_level', help='Level of the analysis that will be p
                     'Multiple participant level analyses can be run independently '
                     '(in parallel) using the same output_dir.',
                     choices=['participant', 'group'])
+parser.add_argument('action', help='train or infer',
+                    choices=['train', 'infer'])
 parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
-                   'corresponds to sub-<participant_label> from the BIDS spec '
-                   '(so it does not include "sub-"). If this parameter is not '
-                   'provided all subjects should be analyzed. Multiple '
-                   'participants can be specified with a space separated list.',
-                   nargs="+")
+                    'corresponds to sub-<participant_label> from the BIDS spec '
+                    '(so it does not include "sub-"). If this parameter is not '
+                    'provided all subjects should be analyzed. Multiple '
+                    'participants can be specified with a space separated list.',
+                    nargs="+")
 parser.add_argument('--skip_bids_validator', help='Whether or not to perform BIDS dataset validation',
-                   action='store_true')
+                    action='store_true')
 parser.add_argument('-v', '--version', action='version',
-                    version='BIDS-App example version {}'.format(__version__))
+                    version='nicMSlesions version {}'.format(__version__))
 
 
 args = parser.parse_args()
 
 if not args.skip_bids_validator:
-    run('bids-validator %s'%args.bids_dir)
+    run('bids-validator %s' % args.bids_dir)
 
 subjects_to_analyze = []
 # only for a subset of subjects
@@ -59,28 +64,51 @@ if args.participant_label:
 # for all subjects
 else:
     subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
-    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+    subjects_to_analyze = [subject_dir.split(
+        "-")[-1] for subject_dir in subject_dirs]
 
+tmpdir = tempfile.mkdtemp()
+config = {
+    'train_folder': tmpdir + args.bids_dir,
+    'inference_folder': tmpdir + args.bids_dir,
+    'test_folder': tmpdir + args.bids_dir,
+    'output_folder': args.output_dir
+}
 # running participant level
 if args.analysis_level == "participant":
 
-    # find all T1s and skullstrip them
+    # find all T1s
     for subject_label in subjects_to_analyze:
-        for T1_file in glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
-                                         "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","anat", "*_T1w.nii*")):
-            out_file = os.path.split(T1_file)[-1].replace("_T1w.", "_brain.")
-            cmd = "bet %s %s"%(T1_file, os.path.join(args.output_dir, out_file))
-            print(cmd)
-            run(cmd)
+        print('subject', subject_label)
+        full_subj_dir = tmpdir + args.bids_dir + "/sub-" + subject_label
+        os.makedirs(full_subj_dir, exist_ok=True)
+        for T1_file in glob(os.path.join(args.bids_dir, "sub-%s" % subject_label,
+                                         "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir, "sub-%s" % subject_label, "ses-*", "anat", "*_T1w.nii*")):
+            try:
+                os.symlink(T1_file, full_subj_dir + "/t1.nii.gz")
+            except FileExistsError:
+                print("Skipping additional run", T1_file,
+                      "for subject", subject_label)
+        for flair_file in glob(os.path.join(args.bids_dir, "sub-%s" % subject_label,
+                                            "anat", "*_FLAIR.nii*")) + glob(os.path.join(args.bids_dir, "sub-%s" % subject_label, "ses-*", "anat", "*_FLAIR.nii*")):
+            try:
+                os.symlink(flair_file, full_subj_dir + "/flair.nii.gz")
+            except FileExistsError:
+                print("Skipping additional run", flair_file,
+                      "for subject", subject_label)
+    fn = 'nic_infer_segmentation_batch' if args.action == "infer" else 'nic_train_network_batch'
+    cmd = "python /nicMSlesions/%s.py" % (fn)
+    run(cmd, config)
 
 # running group level
 elif args.analysis_level == "group":
     brain_sizes = []
     for subject_label in subjects_to_analyze:
-        for brain_file in glob(os.path.join(args.output_dir, "sub-%s*.nii*"%subject_label)):
-            data = nibabel.load(brain_file).get_data()
-            # calcualte average mask size in voxels
-            brain_sizes.append((data != 0).sum())
+        for brain_file in glob(os.path.join(args.output_dir, "sub-%s*.nii*" % subject_label)):
+            try:
+                os.symlink(brain_file, full_subj_dir + "/flair.nii.gz")
+            except FileExistsError:
+                print("Skipping additional run", brain_file,
+                      "for subject", subject_label)
 
-    with open(os.path.join(args.output_dir, "avg_brain_size.txt"), 'w') as fp:
-        fp.write("Average brain size is %g voxels"%numpy.array(brain_sizes).mean())
+shutil.rmtree(tmpdir)
